ca4f0fa70b756074fc3f33f9fd41daaa31751611
Николай <Nicko6k9@yandex.ua>
2020-12-23 04:45:36 +0200

    Add project files.

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(EventDispatcherUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(addEventListener_Function_Delegate_OK)
+		{
+		}
+
+		TEST_METHOD(addEventListener_Method_Delegate_OK)
+		{
+		}
+
+		TEST_METHOD(removeEventListener_Function_Delegate_OK)
+		{
+		}
+
+		TEST_METHOD(removeEventListener_Method_Delegate_OK)
+		{
+		}
+
+		TEST_METHOD(removeEventListener_Function_Delegate_Delegate_Does_Not_Exist)
+		{
+		}
+
+		TEST_METHOD(removeEventListener_Method_Delegate_Delegate_Does_Not_Exist)
+		{
+		}
+	};
+}
